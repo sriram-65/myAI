e25d6f52a8be061addf4c23238911c0ab59c81d6
sriram-65 <159551306+sriram-65@users.noreply.github.com>
2024-07-22 19:26:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-
+import './styles/style.css'
 export default function Home() {
   return (
    <>
-    <div className="flex flex-col justify-center items-center w-full h-screen bg-white">
-      <h1 className="mt-10 text-6xl font-extrabold text-black font-serif">Get Start With Our AI Models</h1>
+    <div className="flex flex-col justify-center items-center w-full h-screen bg-white animated-gradient ">
+      <h1 className="mt-10 text-6xl text-white font-bold m-3">Get Start With Our AI Models</h1>
       <br />
       <br />
-      <p className="font-mono text-black">This Application Was Developed and Hosted By Sriram And Arfan From  Muthu Thevar Mukkulathore HR.Sec School in Thirunagar,Madurai</p>
+      <p className="font-mono text-white font-extrabold m-7">This Application Was Developed and Hosted By Sriram And Arfan From  Muthu Thevar Mukkulathore HR.Sec School in Thirunagar,Madurai</p>
       <br />
       <br />
       <div>
         <Link href={"/dashboard"}>
-       <Button className="p-6" variant={"destructive"}>GET START</Button>
+       <Button className="p-6" >GET START</Button>
        </Link>
       </div>
     
