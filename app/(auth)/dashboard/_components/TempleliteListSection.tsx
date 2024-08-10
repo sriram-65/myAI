2bae65b3846b2de113b2a2df0d43d40c482e1620
sriram-65 <159551306+sriram-65@users.noreply.github.com>
2024-08-10 15:26:58 +0530

    Update TempleliteListSection.tsx

--- app/(auth)/dashboard/_components/TempleliteListSection.tsx
+++ app/(auth)/dashboard/_components/TempleliteListSection.tsx
@@ -47,7 +47,7 @@ function TempleliteListSection({ userSearchInput }: any) {
             
     <h1 className='font-bold text-3xl '>For <span className='font-mono text-yellow-900 shadow-inner'>Image Generation</span></h1>
     <br /> 
-    <Button variant={"destructive"}> <a href='https://sriram-65.github.io/imageGen/' >Get Start Now</a> </Button>
+    <Button > <a href='https://sriram-65.github.io/imageGen/' >Get Start Now</a> </Button>
     
   
             </div>
